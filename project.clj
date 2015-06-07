0a6b545e2866a6e3ca084ef80212df5aafd82393
Gal Dolber <gal.dolber@gmail.com>
2015-06-07 18:56:25 -0300

    Update deps

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject cljobjc "0.1.0-SNAPSHOT"
-  :plugins [[lein-objcbuild "0.1.8"]]
+  :plugins [[lein-objcbuild "0.1.9"]]
   :aot :all
   :objcbuild {:main cljobjc.core/main}
   :dependencies [[galdolber/clojure-objc "1.7.0-RC1"]])
